aeee5e5634889c9d4ac6fcc524795a08c6e9732e
Stefan Karg <mail@stefankarg.de>
2022-02-23 18:13:19 +0100

    Hello World on eink.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 
+
 import digitalio
 import busio
 import board
 from adafruit_epd.epd import Adafruit_EPD
+from PIL import Image, ImageDraw, ImageFont
+
 
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 ecs = digitalio.DigitalInOut(board.CE0)
@@ -11,11 +14,23 @@ rst = digitalio.DigitalInOut(board.D27)
 busy = digitalio.DigitalInOut(board.D17)
 srcs = None
 
+WHITE = (0xFF, 0xFF, 0xFF)
+BLACK = (0x00, 0x00, 0x00)
+FONTSIZE = 24
+
+
 from adafruit_epd.ssd1680 import Adafruit_SSD1680
 display = Adafruit_SSD1680(122, 250, spi, cs_pin=ecs, dc_pin=dc, sramcs_pin=srcs, rst_pin=rst, busy_pin=busy)
 
+display.rotation = 1
+
 display.fill(Adafruit_EPD.WHITE)
-display.fill_rect(0, 0, 50, 60, Adafruit_EPD.BLACK)
-display.hline(80, 30, 60, Adafruit_EPD.BLACK)
-display.vline(80, 30, 60, Adafruit_EPD.BLACK)
+image = Image.new("RGB", (display.width, display.height), color=WHITE)
+draw = ImageDraw.Draw(image)
+
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", FONTSIZE)
+
+# Draw the city
+draw.text((5, 5), "Hello World!", font=font, fill=BLACK)
+display.image(image)
 display.display()

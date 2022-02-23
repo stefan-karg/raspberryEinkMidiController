b378313e9facb3e324d80260250e6da8b1189659
Stefan Karg <mail@stefankarg.de>
2022-02-23 17:58:43 +0100

    Inital e-ink stuff

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import digitalio
+import busio
+import board
+from adafruit_epd.epd import Adafruit_EPD
 
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+ecs = digitalio.DigitalInOut(board.CE0)
+dc = digitalio.DigitalInOut(board.D22)
+rst = digitalio.DigitalInOut(board.D27)
+busy = digitalio.DigitalInOut(board.D17)
+srcs = None
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+from adafruit_epd.ssd1680 import Adafruit_SSD1680
+display = Adafruit_SSD1680(122, 250, spi, cs_pin=ecs, dc_pin=dc, sramcs_pin=srcs, rst_pin=rst, busy_pin=busy)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+display.fill(Adafruit_EPD.WHITE)
+display.fill_rect(0, 0, 50, 60, Adafruit_EPD.BLACK)
+display.hline(80, 30, 60, Adafruit_EPD.BLACK)
+display.vline(80, 30, 60, Adafruit_EPD.BLACK)
+display.display()
